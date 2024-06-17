d57582c04ce1b849108fa79d8f0ac96c9d86bafc
Yaroslav Nikolenko <yaroslavnikolenko@MacBook-Air-Yaroslav.local>
2024-06-17 15:34:54 +0300

    feature: splash screen

--- app/src/main/java/com/pfv/cryptotracker/data/constants/GlobalAppConstants.kt
+++ app/src/main/java/com/pfv/cryptotracker/data/constants/GlobalAppConstants.kt
@@ -3,5 +3,6 @@ package com.pfv.cryptotracker.data.constants
 object GlobalAppConstants {
 
     const val EMPTY_STRING = ""
+    const val SPLASH_DURATION = 3000L
     const val BASE_URL = "https://api.coindesk.com/"
 }
