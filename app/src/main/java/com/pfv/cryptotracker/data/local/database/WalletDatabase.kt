7c827c8163c5d7bd3b4766cca2a6ea7c6cb573a9
Yaroslav Nikolenko <yaroslavnikolenko@MacBook-Air-Yaroslav.local>
2024-06-20 10:04:23 +0300

    feature: add type converters to database room

--- app/src/main/java/com/pfv/cryptotracker/data/local/database/WalletDatabase.kt
+++ app/src/main/java/com/pfv/cryptotracker/data/local/database/WalletDatabase.kt
@@ -2,12 +2,15 @@ package com.pfv.cryptotracker.data.local.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 import com.pfv.cryptotracker.data.local.dao.WalletDao
 import com.pfv.cryptotracker.data.local.entity.BalanceEntity
 import com.pfv.cryptotracker.data.local.entity.BitcoinStateEntity
 import com.pfv.cryptotracker.data.local.entity.TransactionEntity
+import com.pfv.cryptotracker.tools.RoomTypeConverters
 
 @Database(entities = [BitcoinStateEntity::class, BalanceEntity::class, TransactionEntity::class], version = 1)
+@TypeConverters(RoomTypeConverters::class)
 abstract class WalletDatabase : RoomDatabase() {
 
     abstract fun walletDao(): WalletDao
